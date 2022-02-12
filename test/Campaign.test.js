ec66218c4b05e05c3eddf7949ab7130be1a13820
belalelmi <belalelmi@gmail.com>
2022-02-12 05:29:10 -0500

    add beforeEach function to campaign test

--- test/Campaign.test.js
+++ test/Campaign.test.js
@@ -18,4 +18,14 @@ beforeEach(async () => {
     .deploy({ data: compiledFactory.bytecode })
     .send({ from: accounts[0], gas: '1000000' });
 
-})
+  await factory.methods.createCampaign('100').send({
+    from: accounts[0],
+    gas: '1000000'
+  });
+
+  [campaignAddress] = await factory.methods.getDeployedCampaigns().call();
+  campaign = await new web3.eth.Contract(
+    JSON.parse(compiledCampaign.interface),
+    campaignAddress
+  );
+});
