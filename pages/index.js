b2d161d1c1c0b269f5225e1eec4d9b9ed71b5386
belalelmi <belalelmi@gmail.com>
2022-02-12 08:22:06 -0500

    fix deprecated code issue

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,15 @@
-import React from 'react'
+import React, { Component } from 'react';
+import factory from '../ethereum/factory';
 
-export default function index() {
-  return (
-    <div>index</div>
-  )
+class CampaignIndex extends Component {
+  static async getInitialProps() {
+    const campaigns = await factory.methods.getDeployedCampaigns().call();
+    return { campaigns }
+  }
+
+  render() {
+    return <div>{this.props.campaigns[0]}</div>
+  }
 }
+
+export default CampaignIndex;
