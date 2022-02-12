1cbd903e14f0c9497bd96051ab358f7877c7dc3e
belalelmi <belalelmi@gmail.com>
2022-02-12 07:22:53 -0500

    add instance of deployed campaign factory

---
+++ ethereum/factory.js
@@ -0,0 +1,11 @@
+require("dotenv").config({ path: '../.env' });
+
+import web3 from "./web3";
+import CampaignFactory from './build/CampaignFactory.json';
+
+const instance = new web3.eth.Contract(
+  JSON.parse(CampaignFactory.interface),
+  process.env.CONTRACT_ADDRESS
+);
+
+export default instance;
