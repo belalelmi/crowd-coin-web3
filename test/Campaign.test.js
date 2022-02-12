cf3d8d77489feda6e14128f879e758844c374cc4
belalelmi <belalelmi@gmail.com>
2022-02-12 05:31:59 -0500

    setup initial solidity contract test

--- test/Campaign.test.js
+++ test/Campaign.test.js
@@ -29,3 +29,11 @@ beforeEach(async () => {
     campaignAddress
   );
 });
+
+describe('Campaigns', () => {
+  it('deploys a factory and a campaign', () => {
+    assert.ok(factory.options.address);
+    assert.ok(campaign.options.address);
+  });
+
+});
