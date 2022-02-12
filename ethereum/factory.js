8c5fc722c4a651a398adad84f9ac6dfdedfb12ad
belalelmi <belalelmi@gmail.com>
2022-02-12 08:21:49 -0500

    fix deprecated code issue

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -1,11 +1,9 @@
-require("dotenv").config({ path: '../.env' });
-
 import web3 from "./web3";
 import CampaignFactory from './build/CampaignFactory.json';
 
 const instance = new web3.eth.Contract(
   JSON.parse(CampaignFactory.interface),
-  process.env.CONTRACT_ADDRESS
+  '0xE2D7E4bcc7a1DD39672386E6863bD4513B490103'
 );
 
 export default instance;
