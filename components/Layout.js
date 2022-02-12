61e48663786558ea4b0ff3cd7dd4748d886b4352
belalelmi <belalelmi@gmail.com>
2022-02-12 10:19:12 -0500

    add create new campaign feature & connect to MetaMask

---
+++ components/Layout.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Container } from 'semantic-ui-react'
+import Head from 'next/head'
+import Header from './Header'
+
+const Layout = (props) => {
+  return (
+    <Container>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.12/semantic.min.css"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+      </Head>
+      <Header />
+      {props.children}
+
+    </Container>
+  )
+}
+
+export default Layout
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,41 @@
-import React, { Component } from 'react';
-import factory from '../ethereum/factory';
+import React, { Component } from "react";
+import { Button, Card } from "semantic-ui-react";
+import factory from "../ethereum/factory";
+import Layout from "../components/Layout";
 
 class CampaignIndex extends Component {
   static async getInitialProps() {
     const campaigns = await factory.methods.getDeployedCampaigns().call();
-    return { campaigns }
+    return { campaigns };
+  }
+
+  renderCampaigns() {
+    const items = this.props.campaigns.map((address) => {
+      return {
+        header: address,
+        description: <a>View Campaign</a>,
+        fluid: true,
+      };
+    });
+    return <Card.Group items={items} />;
   }
 
   render() {
-    return <div>{this.props.campaigns[0]}</div>
+    return (
+      <Layout>
+        <div>
+
+          <h3>Open Campaigns</h3>
+          <Button
+            floated="right"
+            content="Create Campaign"
+            icon="add circle"
+            primary
+          />
+          {this.renderCampaigns()}
+        </div>
+      </Layout>
+    );
   }
 }
 
