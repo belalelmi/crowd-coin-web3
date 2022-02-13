eb47d1b22bfa7154f2f62e655c9284455266efc5
belalelmi <belalelmi@gmail.com>
2022-02-13 10:59:13 -0500

    add cards group to display current campaign details

--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -1,11 +1,72 @@
-import React, { Component } from 'react';
-import Layout from '../../components/Layout';
+import React, { Component } from 'react'
+import { Card } from 'semantic-ui-react'
+import Layout from '../../components/Layout'
+import Campaign from '../../ethereum/campaign'
+import web3 from '../../ethereum/web3'
 
-export class CampaignShow extends Component {
+class CampaignShow extends Component {
+  static async getInitialProps(props) {
+    const campaign = Campaign(props.query.address)
+    const summary = await campaign.methods.getSummary().call()
+    return {
+      minimumContribution: summary[0],
+      balance: summary[1],
+      requestsCount: summary[2],
+      approversCount: summary[3],
+      manager: summary[4],
+    }
+  }
+
+  renderCards() {
+    const {
+      balance,
+      manager,
+      minimumContribution,
+      requestsCount,
+      approversCount,
+    } = this.props
+
+    const items = [
+      {
+        header: manager,
+        meta: 'Address of Manager',
+        description:
+          'The manager created this campaign and can create requests to withdraw funds.',
+        style: { overflowWrap: 'break-word' },
+      },
+      {
+        header: minimumContribution,
+        meta: 'Minimum Contribution (Wei)',
+        description:
+          'You must contribute at least this much Wei become an approver.',
+      },
+      {
+        header: requestsCount,
+        meta: 'Number of Requests',
+        description:
+          'A request tries to withdraw from the contract. A request must be approved by approvers.',
+      },
+      {
+        header: approversCount,
+        meta: 'Number of Approvers',
+        description:
+          'Numbers of people who have already donated to this campaign.',
+      },
+      {
+        header: web3.utils.fromWei(balance, 'ether'),
+        meta: 'Current Balance (ether)',
+        description: 'The balance is the current funds of a campaign.',
+      },
+    ]
+
+    return <Card.Group items={items} />
+  }
+
   render() {
     return (
       <Layout>
         <h3>CampaignShow</h3>
+        {this.renderCards()}
       </Layout>
     )
   }
